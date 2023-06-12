5ab35f1614c1dccca1914a87531868b786795be9
Rafael Lima <rafael.deabreu.tjii@gmail.com>
2023-06-12 11:21:27 -0300

    Finalização

--- js/atleta.js
+++ js/atleta.js
@@ -13,15 +13,13 @@ fotonova.alt = descricao;
 
 div.appendChild(fotonova);
 
-const nomeElement = document.createElement('h2');
-nomeElement.textContent = nome;
-nomeElement.style.textAlign = 'left';
-div.appendChild(nomeElement);
+const nome_jogador = document.createElement('h3');
+nome_jogador.textContent = `Nome: ${nome}`;
+div.appendChild(nome_jogador);
 
 
-const posicao_info = document.createElement('h3');
+const posicao_info = document.createElement('h4');
 posicao_info.textContent = `Posição: ${posicaoJogo}`;
-posicao_info.style.textAlign = 'left';
 div.appendChild(posicao_info);
 
 
@@ -29,15 +27,15 @@ const nome_completo = document.createElement('h4');
 nome_completo.textContent = `Nome completo: ${nomeCompleto}`;
 div.appendChild(nome_completo);
 
-const descricaoElement = document.createElement('h4');
-descricaoElement.textContent = `Descrição sobre a atleta: ${descricao}`;
-div.appendChild(descricaoElement);
+const descricao_info = document.createElement('h4');
+descricao_info.textContent = `Descrição sobre a atleta: ${descricao}`;
+div.appendChild(descricao_info);
 
 
 
-const dataNascimentoElement = document.createElement('h4');
-dataNascimentoElement.textContent = `Data e local de nascimento: ${dataNascimento}`;
-div.appendChild(dataNascimentoElement);
+const nascimento_info = document.createElement('h4');
+nascimento_info.textContent = `Data e local de nascimento: ${dataNascimento}`;
+div.appendChild(nascimento_info);
 
 const altura_info = document.createElement('h4');
 altura_info.textContent = `Altura: ${alturaJogador}`;
@@ -45,4 +43,10 @@ div.appendChild(altura_info);
 
 body.appendChild(div);
 
-fotonova.classList.add('classeImagem');
+fotonova.className = 'foto_jogador'
+nome_jogador.className = 'itens_jogador';
+posicao_info.className = 'itens_jogador';
+nome_completo.className = 'itens_jogador';
+descricao_info.className = 'itens_jogador';
+nascimento_info.className = 'itens_jogador';
+altura_info.className = 'itens_jogador';
